b451e070ca8a84543fc109e887a71ba88a22a10c
aksonal <41297988+aksonal@users.noreply.github.com>
2025-12-11 01:07:47 +0530

    Create code.py
    code

---
+++ guess_num/code.py
@@ -0,0 +1,6 @@
+target_num = 8
+guess_num = None # initialize
+while(guess_num != target_num):
+    guess_num = int(input('Guess a number between 1 and 10 until you get it right : '))
+
+print("Well Guessed!")
