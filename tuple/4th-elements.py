ba6e5579515e860d7cbee25caf46642c17536448
aksonal <41297988+aksonal@users.noreply.github.com>
2025-12-13 01:13:28 +0530

    Create 4th-elements.py
    code

---
+++ tuple/4th-elements.py
@@ -0,0 +1,7 @@
+#Find the first 4th and last 4th element
+
+tup = ('e','x','e','r','c','i','s','e')
+first_fourth_element = tup[3]
+last_fourth_element = tup[-4]
+
+print(f"first 4th element is {first_fourth_element} and the last fourth element is {last_fourth_element}")
