5d8dc7963021bdefdc0991e417394b302b1361de
aksonal <41297988+aksonal@users.noreply.github.com>
2025-12-14 22:55:47 +0530

    Create count.py
    code

---
+++ File_Handling/count.py
@@ -0,0 +1,9 @@
+#Python code to read no. of times ERROR, WARN or INFO log lines (short files)
+count = 0
+with open('app.log','r') as file:
+# file.write("Hello Worldies!!!")
+    for line in file:
+        if "WARN" in line:
+          count+=1
+
+print(count)
